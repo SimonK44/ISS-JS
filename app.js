9fb571281ee7bf43ac4e2445db072e00249c15b2
simon <s.kervadec@yahoo.fr>
2024-07-31 20:05:37 +0200

    déplacement marker Nantes vers ISS

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-// setInterval(creationMap, 2000);
-creationMap();
+let map;
+let marker;
 
-function creationMap() {
-    fetch('http://api.open-notify.org/iss-now.json')
-        .then(r => r.json())
-        .then(reponse => {
+function init() {
+    initMap();
+    setInterval(getLatLong, 4000);
+}
 
-    let latitude = document.createElement('p');
-    let longitude = document.createElement('p');
-    latitude.innerText = "Latitude = " + reponse["iss_position"]["latitude"];
-    longitude.innerText = "Longitude = " + reponse["iss_position"]["longitude"];
-    document.getElementById('coordonnees').appendChild(latitude);
-    document.getElementById('coordonnees').appendChild(longitude);
-
-    map = L.map('map').setView([51.505, -0.09], 13);        
+function initMap() {
+    map = L.map('map').setView([47.216671, -1.55], 13)    
 
     let station = L.icon({
         iconUrl: 'image/iss.png',
-        iconSize: [38, 95], // size of the icon
-    });
-
-    marker = L.marker([51.505, -0.09], {icon: station}).addTo(map);
+        iconSize: [64, 64],
+        iconAnchor: [32, 32],
+        popupAnchor: [-3, -76]
+    })
+    
+    marker = L.marker([47.216671, -1.55], {icon: station}).addTo(map);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-
-    map.flyTo(([reponse["iss_position"]["latitude"], reponse["iss_position"]["longitude"]]));
-   
-    })
-
 }
 
+function getLatLong() {
+    fetch('http://api.open-notify.org/iss-now.json')
+        .then(r => r.json())
+        .then(json => {
+            const latitude = json["iss_position"]["latitude"];
+            const longitude = json["iss_position"]["longitude"];
+
+            document.getElementById('coordonnees').innerText = "Latitude : "+latitude+" / Longitude : "+longitude;
+
+            marker.setLatLng([latitude,longitude]);
+
+            map.flyTo([latitude,longitude]);
+        })
+};
+
+onload = init;
 
 
